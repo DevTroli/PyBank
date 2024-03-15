0827a8be62e34a550fbba342239b3c4354d64f59
Troli <119256727+DevTroli@users.noreply.github.com>
2024-03-15 18:57:43 -0300

    Add files via upload

--- PyBank.py
+++ PyBank.py
@@ -1,59 +1,167 @@
-menu = '''
+import textwrap
 
-    [d] Depositar
-    [s] Sacar
-    [e] Extrato
-    [q] Sair
+# Função para o menu de opções para o usuário escolher oque quer fazer no PyBank
+def menu():
+    menu = """ 
+    Bem vindo ao PyBank o seu banco pelo seu terminal 
+    ================ MENU ================
+    [d]\tDepositar
+    [s]\tSacar
+    [e]\tExtrato
+    [nc]\tNova conta
+    [lc]\tListar contas
+    [nu]\tNovo usuário
+    [q]\tSair
+    => """
+    return input(textwrap.dedent(menu))
 
-=> '''
+# Função para o usuário fazer o seu depósito sendo os argumentos apenas por posicionamento
+# Para argumentos por posição usei '/' como ultimo argumento assim fazendo o argumento da função ser por posição
+# Foi usado uma estrutura condicional para a logica de sucesso e falha no processo de deposito do usuário
+def depositar(saldo, valor, extrato, /):
+    if valor > 0:
+        saldo += valor
+        extrato += f"Depósito:\tR$ {valor:.2f}\n"
+        print("\n=== Depósito realizado com sucesso! ===")
+    else:
+        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
 
-saldo = 0
-limite = 500
-extrato= ""
-num_saques= 0 
-LIMITE_SAQUES = 3
+    return saldo, extrato
 
-while True:
-    opcao = input(menu)
+# Nessa funçao foi usado variaveis, operadores lógicos, e um fluxo condicional com completo (if,elif,else)
+# A função usa argumentos só pelo nome do argumento e para funcionar o primeiro argumento tem que ser o '*' 
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saques = numero_saques >= limite_saques
 
-    if opcao == "d":
-        valor = float (input("Qual o valor do deósito? "))
+    if excedeu_saldo:
+        print("\n@@@ Operação falhou! Você não tem saldo suficiente. @@@")
 
-        if valor > 0 :
-            saldo += valor
-            extrato += f"Depósito: R$ {valor:.2f}\n"
-        else:
-            print("Deu erro na operação! Valor informado é invlido para prosseguir")
+    elif excedeu_limite:
+        print("\n@@@ Operação falhou! O valor do saque excede o limite. @@@")
 
-    elif opcao == "s":
-        valor = float(input("Qual vai ser o valor do saque? "))
-        
-        ultrapassou_saldo = valor > saldo
-        ultrapassou_limite =  valor > limite
-        ultrapassou_saques = num_saques >= LIMITE_SAQUES
+    elif excedeu_saques:
+        print("\n@@@ Operação falhou! Número máximo de saques excedido. @@@")
 
-        if ultrapassou_saldo:
-            print("Operação Falhou Você não tem saldo suficiente")
-        elif ultrapassou_limite:
-            print("Operação Falhou! Você passou o nosso limite para saques")
-        elif ultrapassou_saques:
-            print("Operação Falhou! Pois você ultrapassou o limite de seus saques diarios")
-            
-        elif valor > 0:
-            saldo = valor
-            extrato += f"saque: R$ {valor:.2f}\n"
-        else:
-            print("Algo deu errado! Pois seu valor inserido é invalido")
-
-    elif opcao == "e":
-        print("\n=========EXTRATO==========")
-        print("Não foi feitas nenhuma movimentação em sua conta." if not extrato else extrato)
-        print(f"\n saldo R$: {saldo:.2f}")
-        print("=========================")
-
-    elif opcao == "q":
-        break
+    elif valor > 0:
+        saldo -= valor
+        extrato += f"Saque:\t\tR$ {valor:.2f}\n"
+        numero_saques += 1
+        print("\n=== Saque realizado com sucesso! ===")
 
     else:
-        print("Operação Inválida, por favor selecione novamente a operação desejada.")
+        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
 
+    return saldo, extrato
+
+# Função que mostrar ao usuário por meios de funções print a movimentação de sua conta no PYBank
+# Os arguemntos da função são um uso misto de 1º posição e em seguida 2º nome do argumento (keyword only)  
+def exibir_extrato(saldo, /, *, extrato):
+    print("\n================ EXTRATO ================")
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print(f"\nSaldo:\t\tR$ {saldo:.2f}")
+    print("==========================================")
+
+# A função usa a funçao input pra pegar a reposta do usuario e fazer o filtro com uma função para a filtragem 
+# Alem dos uso de fluxos condicionais e uso de dicionários para armazenar os dados pessoais do usuário 
+def criar_usuario(usuarios):
+    cpf = input("Informe o CPF (somente número): ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n@@@ Já existe usuário com esse CPF! @@@")
+        return
+
+    nome = input("Informe o nome completo: ")
+    data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
+    endereco = input("Informe o endereço (logradouro, nro - bairro - cidade/sigla estado): ")
+
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
+
+    print("=== Usuário criado com sucesso! ===")
+
+# Função utiliza estruturas de repetição e fluxo condicional para não ter usuarios 'vazios' ou com CPF repetidos
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
+# O input é usado para obter a respota do usuario, depois vem a variavel com uma função de filtro de usuarios
+# Para não ter nenhum erro de logica no sistema do PyBank (Como CPF duplicado ou conta vazia)
+def criar_conta(agencia, numero_conta, usuarios):
+    cpf = input("Informe o CPF do usuário: ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n=== Conta criada com sucesso! ===")
+        return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
+
+    print("\n@@@ Usuário não encontrado, fluxo de criação de conta encerrado! @@@")
+
+# Com o uso de estruturas de repetição essa função lista as contas criadas no PyBank
+def listar_contas(contas):
+    for conta in contas:
+        linha = f"""\
+            Agência:\t{conta['agencia']}
+            C/C:\t\t{conta['numero_conta']}
+            Titular:\t{conta['usuario']['nome']}
+        """
+        print("=" * 100)
+        print(textwrap.dedent(linha))
+
+
+def main():
+    LIMITE_SAQUES = 3
+    AGENCIA = "0001"
+
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    usuarios = []
+    contas = []
+
+    while True:
+        opcao = menu()
+
+        if opcao == "d":
+            valor = float(input("Informe o valor do depósito: "))
+
+            saldo, extrato = depositar(saldo, valor, extrato)
+
+        elif opcao == "s":
+            valor = float(input("Informe o valor do saque: "))
+
+            saldo, extrato = sacar(
+                saldo=saldo,
+                valor=valor,
+                extrato=extrato,
+                limite=limite,
+                numero_saques=numero_saques,
+                limite_saques=LIMITE_SAQUES,
+            )
+
+        elif opcao == "e":
+            exibir_extrato(saldo, extrato=extrato)
+
+        elif opcao == "nu":
+            criar_usuario(usuarios)
+
+        elif opcao == "nc":
+            numero_conta = len(contas) + 1
+            conta = criar_conta(AGENCIA, numero_conta, usuarios)
+
+            if conta:
+                contas.append(conta)
+
+        elif opcao == "lc":
+            listar_contas(contas)
+
+        elif opcao == "q":
+            break
+
+        else:
+            print("Operação inválida, por favor selecione novamente a operação desejada.")
+
+
+main()
